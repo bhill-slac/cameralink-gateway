5a2596d92d2c6359fffc46c64cba87e978e14838
Bruce Hill <bhill@slac.stanford.edu>
2019-11-14 21:58:31 -0800

    Personal setup and launch support.

---
+++ software/launch-gui.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+python scripts/gui.py --camTypeA Opal1000 --defaultFile config/defaults_Opal1000.yml 
--- software/setup_env.sh
+++ software/setup_env.sh
@@ -1,2 +1,3 @@
 # Activate Rogue conda Environment
+source ~bhill/miniconda3/etc/profile.d/conda.sh
 conda activate rogue_env
