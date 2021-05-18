063add30c1a668df3ba44c0adc19d2f08c5cfe97
Bruce Hill <bhill@slac.stanford.edu>
2021-05-18 16:07:43 -0700

    New addressMapDump and other dump files.  Slight update to launch-pydm.sh

--- software/launch-pydm.sh
+++ software/launch-pydm.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
+#
 python scripts/devGui \
-	--camType Opal1000 Opal1000 Opal1000 Opal1000\
+	--camType Opal1000 BaslerAce Opal1000 Opal1000\
 	--defaultFile config/Opal1000.yml \
 	--startupMode False \
 	--standAloneMode False \
 	--enableDump  True \
 	--enLclsI  True \
 	--enLclsII True \
+	--pgp4 True \
 	--guiType PyDM 
